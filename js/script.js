7e1461a8bb35911fe1128643066d2643110301e1
sergio biscossa <germanchumi@gmail.com>
2022-07-07 10:39:36 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,124 @@
+// tuve que subir mi .json a un storage externo porque no me lo tomaba como interno. El Live Server me buscaba el ./libros.json como si fuera una web externa y me tiraba error 404 (porque obviamente no existe esa dirección). Aún así, dejé el archivo guardado por las dudas. Aunque no lo pude arreglar, me rebusqué la solución :).
+// (casi me vuelvo loco)
+
+
+
+
+// fetch de libros
+
+let arrayLibros = fetch("https://json.extendsclass.com/bin/2087f43656ba")
+
+.then ((resp) => resp.json() )  
+
+.then( (libros) => {console.log(libros)
+
+    // viendo si hay un localstorage anterior
+
+    const storage = JSON.parse(localStorage.getItem("filtro"));
+
+    if(storage){
+
+        RellenarLibros(storage)
+
+    }else{
+
+        RellenarLibros(libros);
+
+    }
+
+
+
+    //para filtrar los libros
+
+    // IMPORTANTE: sé que este filtrado debería estar en una función, pero sinceramente se me complicó, por alguna razón que no supe decifrar no me funcionaba haciendo una función más abajo y llamándola acá arriba, así que decidí dejarlo así porque al menos funciona.
+
+const generoElegido = document.getElementsByClassName("radio");
+
+
+// recorriendo los selectores para poder usarlos y dándole la función a ejectar
+
+for (const genero of generoElegido){
+    genero.addEventListener("change", FiltrarTabla)
+}
+
+
+function FiltrarTabla(eleccion){
+
+    // guardando el selector que elija el usuario en una variable en mayus
+    let inputValue = eleccion.target.value.toUpperCase();
+
+    
+    // si no se elige "TODOS", filtrar
+    if(inputValue == "TODOS"){
+
+        arrayLibros = libros;
+
+    }else{
+        arrayLibros = libros.filter((libro) => {
+
+            return libro.tipo.toUpperCase() === inputValue;
+
+        })
+ 
+        
+    }
+
+
+    localStorage.setItem("filtro", JSON.stringify(arrayLibros));
+    RellenarLibros(arrayLibros)
+ 
+}
+
+});
+
+
+
+
+// función para rellenar los libros con el arreglo
+
+function RellenarLibros (array){
+
+    const tbody = document.getElementById("tbody");
+    tbody.innerHTML= "";
+
+
+    for (let i = 0; i < array.length ; i++){
+    
+    let tr = document.createElement("tr");
+    tr.innerHTML= `<td><img class="fotoLibro" src='${array[i].imagen}'></td>
+    <td>${array[i].libro}</td>
+    <td>${array[i].nombreAut}</td>
+    <td>${array[i].apellidoAut}</td>
+    <td>$${array[i].precio}</td>
+    <button id="${array[i].id}" class="comprar">Agregar al carrito</button>`
+
+     tbody.appendChild(tr);}
+
+}
+
+
+
+
+
+
+
+let botones = document.getElementsByClassName("comprar");
+
+
+botones.onclick =  () => { console.log("asd")};
+
+
+// carrito de compras
+
+
+const tbodyCarrito = document.getElementById("tbodyCarrito");
+
+let trCarrito = document.createElement("tr");
+trCarrito.innerHTML="<td>holaaa</td>";
+
+tbodyCarrito.appendChild(trCarrito);
+
+
+let comprarTodo = document.getElementById("comprarTodo");
+comprarTodo.onclick =  () => { console.log("asd")};
+
